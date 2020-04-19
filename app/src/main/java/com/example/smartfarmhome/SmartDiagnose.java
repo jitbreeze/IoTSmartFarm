c845b2af4d0b787156c7661167ac8d32fe6cc17f
jisuk <kmm153@naver.com>
2020-04-19 16:20:18 +0900

    2020-04-19 개발시작

---
+++ app/src/main/java/com/example/smartfarmhome/SmartDiagnose.java
@@ -0,0 +1,14 @@
+package com.example.smartfarmhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmartDiagnose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart_diagnose);
+    }
+}
---
+++ app/src/main/java/com/example/smartfarmhome/ui/mypage/MyPageFragment.java
@@ -0,0 +1,42 @@
+package com.example.smartfarmhome.ui.mypage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.smartfarmhome.R;
+import com.example.smartfarmhome.SmartDiagnose;
+
+public class MyPageFragment extends Fragment {
+    private com.example.smartfarmhome.ui.mypage.MyPageViewModel myPageViewModel;
+
+    Button mp_History; Button mp_SmartDiagnose; Button mp_Info;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        myPageViewModel =
+                ViewModelProviders.of(this).get(com.example.smartfarmhome.ui.mypage.MyPageViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_mypage, container, false);
+        mp_History = root.findViewById(R.id.mp_History);
+        mp_SmartDiagnose = root.findViewById(R.id.mp_SmartDiagnose);
+        mp_Info = root.findViewById(R.id.mp_Info);
+
+        mp_SmartDiagnose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SmartDiagnose.class);
+                startActivity(intent);
+            }
+        });
+        return root;
+    }
+}
